5d9117b8a30865f96e1d671d47d9fa6b15c87750
Donald Knieriem <donald.knieriem@temple.edu>
2020-06-10 16:25:13 -0400

    bring in uikit, fix disabled a.uk-button, build first docs working with twig

---
+++ components/button/button.stories.js
@@ -0,0 +1,40 @@
+import { storiesOf } from '@storybook/html';
+
+import { withActions } from '@storybook/addon-actions';
+import { array, boolean, button, color, date, select, withKnobs, text, number } from '@storybook/addon-knobs';
+
+import buttonTwig from './button.twig'
+
+import buttonScss from './button.scss'
+
+import "../../src/scripts/uikit.min.js";
+import "../../src/styles/uikit/dist/css/uikit.min.css";
+
+const button_types = {
+	Default: 'uk-button-default',
+	Primary: 'uk-button-primary',
+	Secondary: 'uk-button-secondary',
+	Danger: 'uk-button-danger',
+	Text: 'uk-button-text',
+	Link: 'uk-button-link'	
+};
+
+const b_type = select('Type', button_types, 'uk-button-default');
+
+storiesOf('Components|Button', module)
+.addDecorator(withKnobs)
+/*.addDecorator(withActions('click', 'click uk-button'))*/
+.add('Default', () => 
+	buttonTwig({
+		label: 'Default Button', 
+		type: select('Type', button_types, 'uk-button-default'),
+		href: '',
+		disabled: false
+	}))
+
+.add('Disabled', () => buttonTwig({
+	label: 'Disabled Button',
+	type: select('Type', button_types, 'uk-button-default'),
+	href: '',
+	disabled: true
+}));
--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,6 +1,8 @@
 const COMMIT = process.env.COMMIT || 'N.A.'
 const VERSION = process.env.VERSION || 'N.A.'
 
+import uikit from './uikit.min.js'
+
 function ready(init) {
   window.addEventListener('reload', init)
 }

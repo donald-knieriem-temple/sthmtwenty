6f0e2cf27bdf3dd51c84ed5b5ea05732e943d2b5
Donald Knieriem <donald.knieriem@temple.edu>
2020-06-12 16:01:57 -0400

    dropdown in progress

---
+++ components/dropdown/dropdown.js
@@ -0,0 +1,44 @@
+import { storiesOf } from '@storybook/html';
+
+import { withActions } from '@storybook/addon-actions';
+import { array, boolean, button, color, date, select, withKnobs, text, number } from '@storybook/addon-knobs';
+
+import dropdown from './dropdown.twig'
+
+import dropdownScss from './dropdown.scss'
+
+import "../../src/scripts/uikit.min.js";
+import "../../src/styles/uikit/dist/css/uikit.min.css";
+
+storiesOf('Components|Dropdown', module)
+.add('Default', () => 
+	dropdown({
+		buttonLabel: 'Dropdown', 
+		buttonType: 'uk-button-default',
+		items: [
+		{
+			active: true,
+			href: '#',
+			title: 'Item 1'
+		},
+		{
+			navHeader: true,
+			href: '#',
+			title: 'Header'
+		},
+		{
+			href: '#',
+			title: 'Item 2'
+		},
+		{
+			href: '#',
+			title: 'Item 3'
+		},
+		{
+			divider: true		},
+		{
+			href: '#',
+			title: 'Item 4'
+		}
+		]
+}));
